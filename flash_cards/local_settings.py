9cafd784ff4b1e1d2c2535b0c37ae3eb24819ebb
rahulsinh <rahulsinhj9999@gmail.com>
2019-08-29 09:34:40 +0530

    Initial Commit

---
+++ flash_cards/local_settings.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'hbsww#_+jrz4sn9%=u=v&qa-s_jc@ks-4x8p@f62(c--m(3#-!'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'flashcards',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
+}
